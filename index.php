53cb42b5d580e698c5586365228feb4442b073fe
Ray Hollister <github@rayhollister.com>
2022-02-03 11:52:24 -0500

    Update index.php
    Cleaned up old code, and moved UCSID to separate constant to make it easier to edit.

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 
 Plugin name: WJCT Composer Now Playing Shortcode
 Author: Matthew Forgette, Md. Sarwar-A-Kawsar, Ray Hollister
-Version: 1.2
+Version: 1.3
 
 1.1 Working PHP API call
 1.2 Changed API call to JavaScript, and set to run every half hour AND fixed the time format to AP Style!
+1.3 Cleaned up code and moved UCSID
 
 */
 wp_enqueue_style( "composer_now_playing",  plugins_url('style.css', __FILE__) );
@@ -21,8 +22,6 @@ function composer_now_playing_shortcode_callback(){
 	</div>
 
 <script type='text/JavaScript'>
-	// const newElement = document.createElement("div");
-	// document.body.appendChild(newElement);
 
 	let firstRun = true;
 	let minutes;
@@ -30,17 +29,17 @@ function composer_now_playing_shortcode_callback(){
 	"composer-now-playing"
 	);
 
+	// Change this to your station's UCSID number. 	
+	const UCSID = "5187f37be1c838d5f207363f";
+
 	const URL =
-	"https://api.composer.nprstations.org/v1/widget/5187f37be1c838d5f207363f/now?format=json&limit=20";
+	`https://api.composer.nprstations.org/v1/widget/${UCSID}/now?format=json&limit=20`;
 
 	const fetchComposerNowPlaying = async () => {
 	try {
 		const response = await fetch(URL);
 		const data = await response.json();
 		const name = data.onNow.program.name;
-		// const start = "1:00";
-		// const end = "2:00";
-		// const start = data.onNow.start_time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
 		const start = data.onNow.start_utc;
 		let newStart = new Date(start);
 		newStart = newStart.toLocaleTimeString(('en-US'), {hour: 'numeric', minute:'2-digit'});
@@ -49,8 +48,6 @@ function composer_now_playing_shortcode_callback(){
 		let newEnd = new Date(end);
 		newEnd = newEnd.toLocaleTimeString(('en-US'), {hour: 'numeric', minute:'2-digit'});
 		newEnd = newEnd.replace("AM", "a.m.").replace("PM", "p.m.");
-		// const end = data.onNow.end_time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
-		// start and end time converted into preferred format using the moment js (https://momentjs.com/docs/)
 		const program_url = data.onNow.program.program_link;
 
 		element.innerHTML = `
@@ -88,25 +85,6 @@ function composer_now_playing_shortcode_callback(){
 
 	return ob_get_clean();
 }
-
-// function get_program_data(){
-// 	$response = wp_remote_get( 'https://api.composer.nprstations.org/v1/widget/5187f37be1c838d5f207363f/now?format=json&limit=20' );
-// 	$http_code = json_decode(wp_remote_retrieve_body( $response ),true);
-// 	return $http_code;
-// }
-
-// add_shortcode( 'composer_now_playing', 'composer_now_playing_shortcode_callback' );
-// function composer_now_playing_shortcode_callback(){
-// 	ob_start();
-// 	$http_code = get_program_data();
-// 	$program_id = $http_code['onNow']['program_id'];
-// 	$start = date('g:i a',strtotime($http_code['onNow']['start_utc']));
-// 	$end = date('g:i a',strtotime($http_code['onNow']['end_utc']));
-// 	$name = $http_code['onNow']['program']['name'];
-// 	$program_url = $http_code['onNow']['program']['program_link'];
-// 	if (is_null($program_url)){
-// 		$program_url = "/listen";
-// 	}
 	
 // 	return ob_get_clean();
 // }
